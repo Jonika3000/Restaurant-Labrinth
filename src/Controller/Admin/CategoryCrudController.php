0db754bab795cdcc0a6f6df767761bf2da8da77a
Jonika <katyadyduch@gmail.com>
2024-06-11 20:25:25 +0300

    admin category parent field

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Category;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +17,10 @@ class CategoryCrudController extends AbstractCrudController
         return Category::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+       yield TextField::new('name');
+       yield TextareaField::new('description');
+       yield AssociationField::new('parent');
     }
-    */
 }
