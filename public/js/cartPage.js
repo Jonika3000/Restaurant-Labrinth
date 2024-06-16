31142b641af51ca21f8f5fd2f9094a3e571dba1d
Jonika <katyadyduch@gmail.com>
2024-06-16 23:01:57 +0300

    add email to admin

--- public/js/cartPage.js
+++ public/js/cartPage.js
@@ -1,69 +1,71 @@
-    const itemsLocalStorage = localStorage.getItem('cart');
-    const cartItems = itemsLocalStorage ? JSON.parse(itemsLocalStorage) : [];
+var itemsLocalStorage = localStorage.getItem('cart');
+var cartItems = itemsLocalStorage ? JSON.parse(itemsLocalStorage) : [];
+var body = JSON.stringify({ cart: cartItems });
 
-    const body = JSON.stringify({ cart: cartItems });
-
-    fetch('/cart/getItems', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: body
+fetch('/cart/getItems', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: body
+})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
     })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            const itemsContainer = document.getElementById('items');
-            itemsContainer.innerHTML = '';
+    .then(data => {
+        const itemsContainer = document.getElementById('items');
+        itemsContainer.innerHTML = '';
 
-            data.forEach(item => {
-                const newItemDiv = document.createElement('div');
-                newItemDiv.classList.add('bg-background', 'h-full', 'rounded-xl', 'flex', 'md:flex-row', 'gap-3', 'p-4', 'flex-col');
+        data.forEach(item => {
+            const newItemDiv = document.createElement('div');
+            newItemDiv.classList.add('bg-background', 'h-full', 'rounded-xl', 'flex', 'md:flex-row', 'gap-3', 'p-4', 'flex-wrap');
 
-                const imgElement = document.createElement('img');
-                imgElement.classList.add('object-cover', 'rounded-2xl', 'w-full', 'max-h-52', 'max-w-36');
-                imgElement.src = '/uploads/images/' + item.photo;
+            const imgElement = document.createElement('img');
+            imgElement.classList.add('object-cover', 'rounded-2xl', 'w-full', 'max-h-52', 'max-w-36');
+            imgElement.src = '/uploads/images/' + item.photo;
 
-                const textElement = document.createElement('h3');
-                textElement.classList.add('text-white', 'text-2xl', 'font-normal', 'w-full', 'break-words');
-                textElement.textContent = item.name;
+            const textElement = document.createElement('h3');
+            textElement.classList.add('text-white', 'text-2xl', 'font-normal', 'w-full', 'break-words');
+            textElement.textContent = item.name;
 
-                const quantityInput = document.createElement('input');
-                quantityInput.type = 'number';
-                quantityInput.value = item.quantity;
-                quantityInput.classList.add('border', 'rounded-lg', 'w-7', 'p-1', 'h-7');
-                quantityInput.min = 1;
+            const quantityInput = document.createElement('input');
+            quantityInput.type = 'number';
+            quantityInput.value = item.quantity;
+            quantityInput.classList.add('border', 'rounded-lg', 'w-7', 'p-1', 'h-7');
+            quantityInput.min = 1;
 
-                quantityInput.addEventListener('change', function() {
-                    const newQuantity = parseInt(quantityInput.value, 10);
-                    item.quantity = newQuantity;
-                    const index = cartItems.findIndex(cartItem => cartItem.id === item.id);
-                    if (index !== -1) {
-                        cartItems[index].quantity = newQuantity;
-                        localStorage.setItem('cart', JSON.stringify(cartItems));
-                    }
-                });
-
-                newItemDiv.appendChild(imgElement);
-                newItemDiv.appendChild(textElement);
-                newItemDiv.appendChild(quantityInput);
-
-                itemsContainer.appendChild(newItemDiv);
+            quantityInput.addEventListener('change', function() {
+                const newQuantity = parseInt(quantityInput.value, 10);
+                item.quantity = newQuantity;
+                const index = cartItems.findIndex(cartItem => cartItem.id === item.id);
+                if (index !== -1) {
+                    cartItems[index].quantity = newQuantity;
+                    localStorage.setItem('cart', JSON.stringify(cartItems));
+                }
             });
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
+
+            newItemDiv.appendChild(imgElement);
+            newItemDiv.appendChild(textElement);
+            newItemDiv.appendChild(quantityInput);
+
+            itemsContainer.appendChild(newItemDiv);
         });
-
-    const form = document.querySelector('form');
-    form.addEventListener('submit', function(event) {
-
-        const cartInput = document.getElementById('cart-input');
-        cartInput.value = JSON.stringify(cartItems);
-
-        localStorage.removeItem('cart');
+        delete itemsLocalStorage;
+        delete cartItems;
+        delete body;
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
     });
+
+var form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+
+    const cartInput = document.getElementById('cart-input');
+    cartInput.value = JSON.stringify(cartItems);
+    delete form;
+    localStorage.removeItem('cart');
+});
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -4,27 +4,38 @@ namespace App\Controller;
 
 use App\Entity\Order;
 use App\Entity\OrderItem;
+use App\Entity\User;
 use App\Form\OrderFormType;
 use App\Repository\DishRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class CartController extends AbstractController
 {
     private $security;
+    private $entityManager;
+    private $mailer;
+    private $dishRepository;
 
-    public function __construct(Security $security)
+    public function __construct(Security $security, EntityManagerInterface $entityManager, MailerInterface $mailer, DishRepository $dishRepository)
     {
         $this->security = $security;
+        $this->entityManager = $entityManager;
+        $this->mailer = $mailer;
+        $this->dishRepository = $dishRepository;
     }
 
     #[Route('/cart', name: 'app_cart')]
-    public function index(Request $request, EntityManagerInterface $entityManager, DishRepository $dishRepository): Response
+    public function index(Request $request): Response
     {
         $order = new Order();
         $form = $this->createForm(OrderFormType::class, $order);
@@ -40,7 +51,7 @@ class CartController extends AbstractController
                 $cartItems = json_decode($cartData, true);
 
                 foreach ($cartItems as $cartItem) {
-                    $dish = $dishRepository->findOneBy(['id'=>$cartItem['id']]);
+                    $dish = $this->dishRepository->find($cartItem['id']);
                     if ($dish) {
                         $orderItem = new OrderItem();
                         $orderItem->setItem($dish);
@@ -51,8 +62,10 @@ class CartController extends AbstractController
                     }
                 }
             }
-            $entityManager->persist($order);
-            $entityManager->flush();
+            $this->entityManager->persist($order);
+            $this->entityManager->flush();
+
+            $this->notifyAdminsAboutNewOrder($order);
 
             $this->addFlash('success', 'Order successfully made! Our manager will call you back soon.');
             return $this->redirectToRoute('app_cart_success');
@@ -70,7 +83,7 @@ class CartController extends AbstractController
     }
 
     #[Route('/cart/getItems', name: 'cart_items')]
-    public function items(Request $request, DishRepository $dishRepository): JsonResponse
+    public function items(Request $request): JsonResponse
     {
         $content = $request->getContent();
         $data = json_decode($content, true);
@@ -78,25 +91,38 @@ class CartController extends AbstractController
         if (!isset($data['cart']) || !is_array($data['cart'])) {
             return new JsonResponse(['error' => 'Invalid cart data'], 400);
         }
+
         $formattedDishes = [];
 
         foreach ($data['cart'] as $item) {
-            if (isset($item['id']) && isset($item['quantity'])) {
-                $dish = $dishRepository->findOneBy(['id' => $item['id']]);
-                if ($dish) {
-                    $formattedDishes[] = [
-                        'id' => $dish->getId(),
-                        'name' => $dish->getName(),
-                        'description' => $dish->getDescription(),
-                        'price' => $dish->getPrice(),
-                        'discount' => $dish->getDiscount(),
-                        'photo' => $dish->getPhoto(),
-                        'quantity' => $item['quantity'],
-                    ];
-                }
+            $dish = $this->dishRepository->find($item['id']);
+            if ($dish) {
+                $formattedDishes[] = [
+                    'id' => $dish->getId(),
+                    'name' => $dish->getName(),
+                    'description' => $dish->getDescription(),
+                    'price' => $dish->getPrice(),
+                    'discount' => $dish->getDiscount(),
+                    'photo' => $dish->getPhoto(),
+                    'quantity' => $item['quantity'],
+                ];
             }
         }
+
         return new JsonResponse($formattedDishes);
     }
 
+    private function notifyAdminsAboutNewOrder(Order $order): void
+    {
+//        $adminEmail = $this->getParameter('admin_email');
+//
+//        $email = (new NotificationEmail())
+//            ->subject('New order')
+//            ->htmlTemplate('email/order_notification.html.twig')
+//            ->from(new Address($this->getParameter('email_from')))
+//            ->to(new Address($adminEmail))
+//            ->context(['order' => $order]);
+//
+//        $this->mailer->send($email);
+    }
 }
